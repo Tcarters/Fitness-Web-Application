5904a04702c795ef17cf63e23c2a75e3e94ab8e9
Tcarters <cartersedmund@gmail.com>
2022-06-18 05:08:26 +0530

    setup & folder structure done

---
+++ install.sh
@@ -0,0 +1,13 @@
+Step1: npx create-react-app ./
+
+Step 2: Remove existing `src` folder and create a new one `mkdir src`
+
+Step 3: Create a new index.js file.
+
+Step 4: Create the `App.js` file
+
+Step 5: Replace installed package json depencies with the content of `imported-package` file.
+
+Step 6: Install it with: npm install --legacy-peer-deps
+
+Step 7: Create the `App.css` file && Import it to the App.js file
